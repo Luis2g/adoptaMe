7ebc89df37460034e0039d79013a92b0d373b722
Luis2g <luisjimenez2g@gmail.com>
2022-03-15 08:25:14 -0600

    primer commit

---
+++ adoptaMe/src/main/java/mx/edu/utez/adoptaMe/AdoptaMeApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.adoptaMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaMeApplication.class, args);
+	}
+
+}

8f3b6c343a03bbef8c03e32fcc676027ca43ce40
SaraMCG <20193tn008@utez.edu.mx>
2022-03-17 10:53:06 -0600

    [UPDATE]
    Updating personality and color service plus, adding PetController and createPet.html

--- adoptaMe/src/main/java/mx/edu/utez/adoptaMe/service/PersonalityServiceImpl.java
+++ adoptaMe/src/main/java/mx/edu/utez/adoptaMe/service/PersonalityServiceImpl.java
@@ -2,16 +2,21 @@ package mx.edu.utez.adoptaMe.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mx.edu.utez.adoptaMe.entity.Personality;
+import mx.edu.utez.adoptaMe.repository.PersonalityRepository;
 
 @Service
 public class PersonalityServiceImpl implements PersonalityService{
 
+    @Autowired
+    private PersonalityRepository personalityRepository;
+
     @Override
     public List<Personality> listAll() {
-        return null;
+        return personalityRepository.findAll();
     }
 
     @Override

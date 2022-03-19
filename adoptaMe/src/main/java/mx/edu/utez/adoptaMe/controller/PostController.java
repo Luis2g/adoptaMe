b9f45e900e9457d90e1251f2294fab4ffa1431dd
SebastianSota <74997686+SebastianSota@users.noreply.github.com>
2022-03-18 19:15:33 -0600

    [UPDATE] Post register
    Añadiendo el registro del post

--- adoptaMe/src/main/java/mx/edu/utez/adoptaMe/controller/PostController.java
+++ adoptaMe/src/main/java/mx/edu/utez/adoptaMe/controller/PostController.java
@@ -4,7 +4,9 @@ package mx.edu.utez.adoptaMe.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import mx.edu.utez.adoptaMe.entity.Post;
@@ -16,16 +18,21 @@ public class PostController {
     @Autowired
     private PostServiceImpl postServiceImpl;
 
+    @GetMapping("/modals")
+    public String modals(Post post){
+
+        return "inicioModals";
+    }
+
     @PostMapping("/savePost")
-    public String savePet(Post post, Model model, RedirectAttributes redirectAttributes){
-        boolean response = postServiceImpl.save(post);
+    public String savePet(@ModelAttribute("post") Post post, Model model, RedirectAttributes redirectAttributes){
+        boolean response = postServiceImpl.save(post);        
         if(response){
-			// redirectAttributes.addFlashAttribute("msg_success", "¡Registro exitoso!");
-			return "/mascotas/petsList";
-		} else {
-			// redirectAttributes.addFlashAttribute("msg_error", "¡Registro fallido!");
-			return "/mascotas/createPet";
-		}
+            redirectAttributes.addFlashAttribute("msg_success", "Registro exitoso");
+            return "redirect:/noticias";
+        }else{
+            return "redirect:/inicioModals";
+        }
     }
 
 }
